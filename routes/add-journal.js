5ddd9960d1a11bcd6aadadc5ea04d78629cda074
trini7y <okekedesmond71@gmail.com>
2023-02-18 22:11:58 +0100

    Styling the card

--- routes/add-journal.js
+++ routes/add-journal.js
@@ -7,8 +7,16 @@ route.get('/add-journal', (req, res, next) => {
     res.render('add-journal', {pageTitle:'Add-Journal'})
 });
 
-route.post('/add-journal', (req, res, net) => {
-    journal.push({title: req.body.title, desc:req.body.description})
+
+
+route.post('/add-journal', (req, res, next) => {
+    function identity() {
+      i = 0;
+      return i+=1;
+    }
+    console.log( identity() );
+
+    journal.push({id: identity(), title: req.body.title, desc:req.body.description})
     console.log(journal)
     res.redirect('/')
 });
